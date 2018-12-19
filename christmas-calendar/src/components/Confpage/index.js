7b92905706edbde1129807bff132005d90df2bfb
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-12-19 18:04:05 +0100

    text-info apparait/dispparait

--- christmas-calendar/src/components/Confpage/index.js
+++ christmas-calendar/src/components/Confpage/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './index.css';
 
-class Confpage extends component{
-
-    handleClick(){
-       
+class Confpage extends Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            showTextInfo: 'hidden'
+        }
     }
+
+    toggleInfo(){
+        let css = (this.state.showTextInfo === 'hidden') ? 'show' : 'hidden';
+        this.setState({showTextInfo:css});
+    }
+
     render(){
         return(
-            <div>
-                <button onClick={(ev) => this.handleClick(ev)}>Next</button>
+            <div className='container'>
+                <div className='text_info'>
+                <p className={this.state.showTextInfo}>LIEN CALENDAR</p>
+                </div>
+                <button onClick={(ev) => this.toggleInfo(ev)}>Next</button>
             </div>
         )
     }
