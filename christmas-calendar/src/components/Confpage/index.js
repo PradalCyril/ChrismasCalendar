de92f3d7c2d17fa66b264e9ba42e800e99236b5c
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-12-19 16:10:16 +0100

    configPage en cours

--- christmas-calendar/src/components/Confpage/index.js
+++ christmas-calendar/src/components/Confpage/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class Confpage extends component{
+
+    handleClick(){
+       
+    }
     render(){
         return(
             <div>
-
+                <button onClick={(ev) => this.handleClick(ev)}>Next</button>
             </div>
         )
     }
