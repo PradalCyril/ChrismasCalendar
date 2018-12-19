e1c355e9aff6bc6acd5567caa74c3358dc77fd79
Olivier.Sohy <gorgobox@gmail.com>
2018-12-19 18:07:14 +0100

    index

---
+++ christmas-calendar/src/components/url_calendar/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+const connection = require('./conf');
+
+
+
+app.get('/calendar/id', (req, res) => {
+
+    // connection à la base de données, et sélection des employés
+    connection.query('SELECT * from calendar', (err, results) => {
+  
+      if (err) {
+  
+        res.status(500).send('Erreur lors de la récupération des infos');
+      } else {
+  
+        res.json(results);
+      }
+    });
+  });
+
+
+
+
+class Link extends Component {
+
+
+
+    render(){
+        return(
+            <div>
+                    <p>
+                        Congratulation ! Your calendar has been successfully created !
+                        Feel free to share it with your friends !
+                        Your link : http://localhost:3000/calendar/id
+                    </p>
+            </div>
+        )
+    }
+}
+
+export default Link;
+
+
+
+
+
+
