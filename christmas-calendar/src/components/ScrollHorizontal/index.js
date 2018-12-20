d112310ac68cc2e3ee1dbf857fbe03c5ece1b32f
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-12-20 17:14:30 +0100

    manque responsive 2

--- christmas-calendar/src/components/ScrollHorizontal/index.js
+++ christmas-calendar/src/components/ScrollHorizontal/index.js
@@ -14,29 +14,29 @@ class ScrollHorizontal extends Component {
             <div id="container">
                 <div id="container1">
                     <div id="container2">
-                        <div className="boules boule11"><img className="boule" src={Boule1} alt="boule5"/></div>
-                        <div className="boules boule12"><img className="boule" src={Boule2} alt="boule5"/></div>
-                        <div className="boules boule13"><img className="boule" src={Boule3} alt="boule5"/></div>
+                        <div className="boules boule1"><img className="boule" src={Boule1} alt="boule5"/></div>
+                        <div className="boules boule2"><img className="boule" src={Boule2} alt="boule5"/></div>
+                        <div className="boules boule3 bouleRouge"><img className="boule" src={Boule3} alt="boule5"/></div>
                         <div className="boules boule4"><img className="boule" src={Boule4} alt="boule4"/></div>
                         <div className="boules boule5"><img className="boule" src={Boule5} alt="boule5"/></div>
                         <div className="boules boule6"><img className="boule" src={Boule1} alt="boule5"/></div>
                         <div className="boules boule7"><img className="boule" src={Boule2} alt="boule5"/></div>
-                        <div className="boules boule8"><img className="boule" src={Boule3} alt="boule5"/></div>
+                        <div className="boules boule8 bouleRouge"><img className="boule" src={Boule3} alt="boule5"/></div>
                         <div className="boules boule9"><img className="boule" src={Boule4} alt="boule5"/></div>
                         <div className="boules boule10"><img className="boule" src={Boule5} alt="boule5"/></div>
                         <div className="boules boule11"><img className="boule" src={Boule1} alt="boule5"/></div>
                         <div className="boules boule12"><img className="boule" src={Boule2} alt="boule5"/></div>
-                        <div className="boules boule13"><img className="boule" src={Boule3} alt="boule5"/></div>
+                        <div className="boules boule13 bouleRouge"><img className="boule" src={Boule3} alt="boule5"/></div>
                         <div className="boules boule14"><img className="boule" src={Boule4} alt="boule5"/></div>
                         <div className="boules boule15"><img className="boule" src={Boule5} alt="boule5"/></div>
                         <div className="boules boule16"><img className="boule" src={Boule1} alt="boule5"/></div>
                         <div className="boules boule17"><img className="boule" src={Boule2} alt="boule5"/></div>
-                        <div className="boules boule18"><img className="boule" src={Boule3} alt="boule5"/></div>
+                        <div className="boules boule18 bouleRouge"><img className="boule" src={Boule3} alt="boule5"/></div>
                         <div className="boules boule19"><img className="boule" src={Boule4} alt="boule5"/></div>
                         <div className="boules boule20"><img className="boule" src={Boule5} alt="boule5"/></div>
                         <div className="boules boule21"><img className="boule" src={Boule1} alt="boule5"/></div>
                         <div className="boules boule22"><img className="boule" src={Boule2} alt="boule5"/></div>
-                        <div className="boules boule23"><img className="boule" src={Boule3} alt="boule5"/></div>
+                        <div className="boules boule23 bouleRouge"><img className="boule" src={Boule3} alt="boule5"/></div>
                         <div className="boules boule24"><img className="boule" src={Boule6} alt="boule5"/></div>
                     </div>
                 </div>
