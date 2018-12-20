cf8bde1c9b2e9d47aa76799e378fec69cabf70a9
PradalCyril <pradal.cyril@gmail.com>
2018-12-20 19:12:00 +0100

    avancement de la question quand on clique et input

--- christmas-calendar/src/components/Prespage/index.js
+++ christmas-calendar/src/components/Prespage/index.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
+import Link from '../url_calendar/index'
+import logo from '../../logo.svg';
 
 
-class Prespage extends Component{
-    render(){
-        return(
+class Prespage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            qa:[],
+            objectOfTheDay: []
+        }
+    }
+    componentDidMount() {
+        console.log("ID Calendrier : ", this.props.match.params.idCalendar)
+    }
+
+    componentDidUpdate() {
+        console.log("QA :", this.state.qa)
+        console.log("object", this.state.objectOfTheDay)
+    }
+    componentWillMount() {
+        fetch(`http://localhost:3000/api/QA?id=${this.props.match.params.idCalendar}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    qa: data,
+                })
+            }
+            )
+    }
+    handleClick(number) {
+        const objectOfTheDay = this.state.qa.filter(data => data.day == number)
+        this.setState({
+            objectOfTheDay: objectOfTheDay
+        })
+
+    }
+    isTrue(ev) {
+        if (ev.target.value == this.state.objectOfTheDay[0].answer) {
+            console.log('yes')
+        }
+    }
+    render() {
+        return (
             <div>
-
+                <img src={logo} alt="logo" onClick={() => this.handleClick(2)}/>
+                <input onChange={(ev) => this.isTrue(ev)} />
             </div>
         )
     }
